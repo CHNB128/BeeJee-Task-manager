79d5664faee81c313901c1c06d61843dfe84f119
drake.aren <drake.aren@protonmail.com>
2019-02-05 09:27:11 +0600

    add main index file

---
+++ src/view/login/index.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="/view/login/main.css">
+
+<div id="login">
+  <form class="card form-signin" action="/">
+    <input hidden name="page" value="authorization">
+
+    <label for="login" class="sr-only">Login</label>
+    <input name="login" type="login" class="form-control" placeholder="Login" required autofocus>
+
+    <label for="password" class="sr-only">Password</label>
+    <input name="password" type="password" class="form-control" placeholder="Password" required>
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Login in</button>
+  </form>
+</div>
